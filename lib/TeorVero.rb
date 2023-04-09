ab8d30d995f45bcb711e25d1ee2c30b75e2e2a97
AvakyanK <129741273+AvakyanK@users.noreply.github.com>
2023-04-10 01:53:46 +0300

    TeorVer

---
+++ lib/TeorVero.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "TeorVero/version"
+
+module TeorVero
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_TeorVero.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestTeorVero < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::TeorVero::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
