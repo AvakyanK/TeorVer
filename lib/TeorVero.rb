207773d1bbf951fd28b0122e9dc43f1b285f8d52
DashaMirzoian <129742023+DashaMirzoian@users.noreply.github.com>
2023-04-16 22:37:45 +0300

    Update TeorVero.rb

--- lib/TeorVero.rb
+++ lib/TeorVero.rb
@@ -345,6 +345,15 @@ end
   end
 end
    
+   #дисперсия биномиальной переменной
+def self.binDisper(n, p)
+  if (n.is_a? Integer) and (n > 0) and (p.is_a? Float) and (p <= 1) and (p > 0) then
+    return n * p*(1 - p)
+  else
+    return "Data entered incorrectly"
+  end
+end
+   
   #Дальше код
 end
 
